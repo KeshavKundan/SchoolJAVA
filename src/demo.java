9b52669793d87b86d1c987960c29b14dabadde97
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-02-09 21:19:44 +0530

    modified

--- src/demo.java
+++ src/demo.java
@@ -1,23 +1,6 @@
 public class demo {
-    public void DeciBin(int n) {
-        if (n > 0) {
-            DeciBin(n/2);
-            System.out.print(n%2);
-        }
-        else
-            System.out.print(0);
-    }
-    
-    public void DeciOct(int n) {
-        if (n > 0) {
-            DeciOct(n/8);
-            System.out.print(n%8);
-        }
-        else
-            System.out.print(0);
-    }
-    
     public static void main(String args[]) {
-        new demo().DeciOct(11);
+        String x = "Hi Bye Baby don't cry";
+        System.out.print(x.substring(0,8));
     }
 }
