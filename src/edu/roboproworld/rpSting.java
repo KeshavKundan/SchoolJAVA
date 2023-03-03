1d3624dbab2b7d2cd5934ca0f2da26100721a6b3
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-03-03 21:12:07 +0530

    modified

--- src/edu/roboproworld/rpSting.java
+++ src/edu/roboproworld/rpSting.java
@@ -1,13 +1,45 @@
 package edu.roboproworld;
 
 public class rpSting {
-    public int startsWithVowel(String str) {
-        int freq = 0;
+    public int letters(String str) {
+        int c = 0;
+        for (int i = 0; i < str.length(); i++)
+            if (Character.isLetter(str.charAt(i)))
+                c++;
+        return c;
+    }
+
+    public int upperCases(String str) {
+        int c = 0;
+        for (int i = 0; i < str.length(); i++)
+            if (Character.isUpperCase(str.charAt(i)))
+                c++;
+        return c;
+    }
+
+    public int lowerCases(String str) {
+        int c = 0;
+        for (int i = 0; i < str.length(); i++)
+            if (Character.isLowerCase(str.charAt(i)))
+                c++;
+        return c;
+    }
+
+    public int digits(String str) {
+        int c = 0;
+        for (int i = 0; i < str.length(); i++)
+            if (Character.isDigit(str.charAt(i)))
+                c++;
+        return c;
+    }
+
+    public int vowels(String str) {
+        int c = 0;
         for (int i = 0; i < str.length(); i++)
             if (str.charAt(i) == ' ' && (str.toLowerCase().charAt(i + 1) == 'a'
                     || str.toLowerCase().charAt(i + 1) == 'e' || str.toLowerCase().charAt(i + 1) == 'i'
                     || str.toLowerCase().charAt(i + 1) == 'o' || str.toLowerCase().charAt(i + 1) == 'u'))
-                freq++;
-        return freq;
+                c++;
+        return c;
     }
 }
