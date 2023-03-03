1f326b945f445a1784b06b82fb9afe392ab85e8f
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-03-03 20:53:04 +0530

    modified

--- src/App.java
+++ src/App.java
@@ -1,11 +1,5 @@
 import edu.roboproworld.*;
 
 public class App {
-    public static void main(String[] args) {
-        rpNumber number = new rpNumber();
-        int arr[] = number.alphaFrequency("Every Programmer is an Author.");
-        for(int i = 0; i < 26; i++){
-            System.out.println(arr[i]);
-        }
-    }
+    public static void main(String[] args) {}
 }
--- src/edu/roboproworld/rpSort.java
+++ src/edu/roboproworld/rpSort.java
@@ -1,81 +1,84 @@
 package edu.roboproworld;
 
 public class rpSort {
-    public long[] bubble(long arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
+	public void bubble(int arr[]) {
+		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - 1 - i; j++) {
 				if (arr[j] > arr[j + 1]) {
-					long temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
+					arr[j] = arr[j] + arr[j + 1];
+					arr[j + 1] = arr[j] - arr[j + 1];
+					arr[j] = arr[j] - arr[j + 1];
 				}
 			}
 		}
-        return arr;
 	}
 
-    public long[] bubbleD(long arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-			for (int j = 0; j < arr.length - 1 - i; j++) {
-				if (arr[j] < arr[j + 1]) {
-					long temp = arr[j + 1];
-					arr[j + 1] = arr[j];
-					arr[j] = temp;
-				}
+	public void selection(int arr[]) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int index = i;
+			for (int j = i + 1; j < arr.length; j++)
+				if (arr[index] < arr[j])
+					index = j;
+			arr[index] = arr[index] + arr[i];
+			arr[i] = arr[index] - arr[i];
+			arr[index] = arr[index] - arr[i];
+		}
+	}
+
+	public void insertion(int arr[]) {
+		for (int i = 1; i < arr.length; i++) {
+			int j = i - 1;
+			while (j >= 0 && arr[j] > arr[j + 1]) {
+				arr[j] = arr[j] + arr[j + 1];
+				arr[j + 1] = arr[j] - arr[j + 1];
+				arr[j] = arr[j] - arr[j + 1];
+				j--;
 			}
 		}
-        return arr;
 	}
 
-    public long[] selection(long arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] < arr[index])
-                    index = j;
-            long temp = arr[index];
-            arr[index] = arr[i];
-            arr[i] = temp;
-        }
-        return arr;
-    }
+	public void merge(int arr[], int l, int r) {
+		if (l >= r)
+			return;
+		int m = l + (r - l) / 2, merged[] = new int[r - l + 1], i1 = l, i2 = m + 1, x = 0;
+		merge(arr, l, m);
+		merge(arr, m + 1, r);
+		while (i1 <= m && i2 <= r)
+			if (arr[i1] <= arr[i2])
+				merged[x++] = arr[i1++];
+			else
+				merged[x++] = arr[i2++];
+		while (i1 <= m)
+			merged[x++] = arr[i1++];
+		while (i2 <= r)
+			merged[x++] = arr[i2++];
+		for (int i = 0, j = l; i < merged.length; i++, j++)
+			arr[j] = merged[i];
+	}
 
-    public long[] selectionD(long arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] > arr[index])
-                    index = j;
-            long temp = arr[index];
-            arr[index] = arr[i];
-            arr[i] = temp;
-        }
-        return arr;
-    }
+	public int[] primeNum(int arr[]) {
+		int P[] = new int[arr.length], i = 0, j = 0, c;
+		for (; i < 10; i++) {
+			c = 0;
+			for (int f = 2; f < arr[i]; f++)
+				if (arr[i] % f == 0)
+					c++;
+			if (!(c > 0))
+				P[j++] = arr[i];
+		}
+		return P;
+	}
 
-    public int[] primeNum(int arr[]) {
-        int P[] = new int[arr.length], i = 0, j = 0, c;
-        for (; i < 10; i++) {
-            c = 0;
-            for (int f = 2; f < arr[i]; f++)
-                if (arr[i] % f == 0)
-                    c++;
-            if (!(c > 0))
-                P[j++] = arr[i];
-        }
-        return P;
-    }
-
-    public int[] compositeNum(int arr[]) {
-        int C[] = new int[arr.length], i = 0, k = 0, c;
-        for (; i < 10; i++) {
-            c = 0;
-            for (int f = 2; f < arr[i]; f++)
-                if (arr[i] % f == 0)
-                    c++;
-            if (c > 0)
-                C[k++] = arr[i];
-        }
-        return C;
-    }
+	public int[] compositeNum(int arr[]) {
+		int C[] = new int[arr.length], i = 0, k = 0, c;
+		for (; i < 10; i++) {
+			c = 0;
+			for (int f = 2; f < arr[i]; f++)
+				if (arr[i] % f == 0)
+					c++;
+			if (c > 0)
+				C[k++] = arr[i];
+		}
+		return C;
+	}
 }
