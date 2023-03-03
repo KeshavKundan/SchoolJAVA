392dd5ae97202721e2e33742262d7c47cf57d325
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-03-03 20:55:49 +0530

    modified:   src/App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 import edu.roboproworld.*;
 
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
