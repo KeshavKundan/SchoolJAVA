9b1930a4096db51553a3f52bcf96ce90e2888fff
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-03-24 16:57:08 +0530

    added: ArrangeLetters program

--- src/edu/example/ArrangeLetters.java
+++ src/edu/example/ArrangeLetters.java
@@ -1,9 +1,10 @@
+package edu.example;
 
 // wap to input 25 alphabets and store in alphaay alpha[].
 // alphaange all letters in alphabetical order using bubble sort.
 import java.util.Scanner;
 
-public class temp {
+public class ArrangeLetters {
     public static void main(String[] args) {
 
         char alpha[] = new char[25];
@@ -15,7 +16,7 @@ public class temp {
         }
         for (int i = 0; i < alpha.length - 1; i++) {
             for (int j = 0; j < alpha.length - 1 - i; j++) {
-                if ((int) (alpha[j].toLowerCase()) > (int) (alpha[j + 1].toLowerCase())) {
+                if ((int) (Character.toLowerCase(alpha[j])) > (int) (Character.toLowerCase(alpha[j + 1]))) {
                     char temp = alpha[j];
                     alpha[j] = alpha[j + 1];
                     alpha[j + 1] = temp;
